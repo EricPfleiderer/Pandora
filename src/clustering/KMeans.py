55c24e1fdb7c6015d81b2d004b56354b895aaeee
anon <e.pfleiderer@hotmail.com>
2023-03-25 16:07:48 -0400

    Adding kmeans clustering, multivariate gaussian sampling

---
+++ src/clustering/KMeans.py
@@ -0,0 +1,54 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class KMeans:
+
+    def __init__(self, data: np.ndarray, n_means: int, init: str = 'forgy'):
+
+        self.data: np.ndarray = data
+        self.n_means: int = n_means
+        self.init: str = init
+        self.means: np.ndarray = np.zeros(shape=n_means)
+        self.history: np.ndarray = np.expand_dims(np.zeros(data.shape[0]), axis=0)
+
+        # Forgy initialization
+        if init is 'forgy':
+            indexes = np.arange(0, data.shape[0])
+            self.means = data[np.random.choice(indexes, size=n_means)]
+
+        # Assume random partition initialization
+        else:
+            cluster_idx = np.random.random_integers(0, n_means, size=data.shape[0])
+            for n in range(n_means):
+                self.means[n] = data[np.where(cluster_idx == n)]
+
+        converged: bool = False
+        while not converged:
+            min_dist_idx = self.assign()
+            converged = self.check_convergence(min_dist_idx)
+            if converged:
+                self.history = self.history[1:]
+                break
+            self.history = np.concatenate((self.history, np.expand_dims(min_dist_idx, axis=0)))
+            self.update(min_dist_idx)
+
+    def assign(self):
+        distances = np.transpose(np.array([np.linalg.norm(self.data - self.means[n], axis=1) for n in range(self.n_means)]))
+        return np.argmin(distances, axis=1)
+
+    def update(self, min_dist_idx):
+        for n in range(self.n_means):
+            self.means[n] = np.mean(self.data[np.where(min_dist_idx == n)], axis=0)
+
+    def check_convergence(self, min_dist_idx):
+        return np.all(np.equal(min_dist_idx, self.history[-1]))
+
+    def save_experiment(self, path):
+        for idx, snapshot in enumerate(self.history):
+            plt.figure()
+            for n in range(self.n_means):
+                plt.scatter(self.data[np.where(snapshot == n), 0], self.data[np.where(snapshot == n), 1])
+                plt.title(f'Iteration #{idx}')
+                plt.savefig(f'{path}iter{idx}.png')
+
---
+++ src/data.py
@@ -0,0 +1,30 @@
+import numpy as np
+from typing import Tuple
+
+
+def sample_multinormals(n_dists: int = 5, n_points: int = 20, dims: int = 2, loc_bounds: Tuple[float, float] = (-5, 5),
+                        scale_bounds: Tuple[float, float] = (0.5, 1)) -> Tuple[np.ndarray, np.ndarray]:
+
+    """
+    Samples 'n_points' points from 'n_dists' gaussian distributions of dimensions 'dims', each with bounded and
+    uniformly sampled location and scale.
+
+    :param n_dists: Number of gaussian distributions.
+    :param n_points: Number of points generated for each distribution.
+    :param dims: Dimensionality of the data.
+    :param loc_bounds: Bounds of the location parameter of the gaussian distributions.
+    :param scale_bounds: Bounds of the scale parameter of the gaussian distributions.
+    :return:
+    """
+
+    # Location of the barycenter for our synthetic data distributions
+    dist_locs = np.random.uniform(loc_bounds[0], loc_bounds[1], size=(n_dists, dims))
+    synth_data = np.empty((n_dists, n_points, dims))
+
+    for idx, loc in enumerate(dist_locs):
+        synth_data[idx] = np.random.normal(loc=loc, scale=np.random.uniform(scale_bounds[0], scale_bounds[1]),
+                                           size=(n_points, dims))
+
+    random_data = np.random.permutation(synth_data.reshape((n_dists*n_points, dims)))
+
+    return synth_data, random_data
